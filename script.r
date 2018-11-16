4bf3a66beb5fdcbe170843763401098ca02bccc4
Victor Miranda <vmm@cin.ufpe.br>
2018-11-16 02:55:21 -0200

    first ommit

---
+++ script.r
@@ -0,0 +1,25 @@
+# Victor Miranda (vmm)
+
+# início da questão (1) ---------------------------------
+sandlerdf <- read.csv(file="sandler.csv", header=TRUE, sep=",")
+print(sandlerdf)
+
+# início da questão (2) ---------------------------------
+# início da questão (3) ---------------------------------
+# início da questão (4) ---------------------------------
+# início da questão (5) ---------------------------------
+# início da questão (6) ---------------------------------
+# início da questão (7) ---------------------------------
+# início da questão (8) ---------------------------------
+# início da questão (9) ---------------------------------
+
+#Descarregue o arquivo .csv da planilha e imprima o dataframe obtido exatamente do jeito que ele se encontra.
+#Encontre a média das notas (sem utilizar a função pronta do R).
+#Encontre o desvio padrão das notas (sem utilizar a função pronta do R).
+#Encontre a moda das notas (sem utilizar a função pronta do R).
+#Faça uma função que retorna apenas os nomes dos filmes que possuem notas maiores ou iguais a seis (6).
+#Faça uma função que retorna quantos filmes possuem notas abaixo de seis (6).
+#Faça uma função que retorna o nome do filme com menor pontuação e o nome do filme com maior pontuação, nessa ordem. Por fim, faça um dataframe com os dois filmes encontrados com as colunas TÍTULO, NOTA, ANO assim como o original.
+#Faça uma função que retorne o ano em que saíram mais filmes com notas acima de seis e meio (6,5).
+#Faça um histograma onde mostra a frequência de filmes com notas maiores ou iguais a seis de cada ano. Não esqueça de dar um título e fazer ele de forma colorida, facilitando a visualização. Um exemplo hipotético seria:
+  
